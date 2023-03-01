89a451614799b80f58d9510d6297d1bbffd2d768
twqueen <tiffanyytsai@gmail.com>
2023-03-01 00:56:15 -0800

    updated test and index js

--- index.js
+++ index.js
@@ -36,3 +36,43 @@ const questions = [
     },
 ];
 
+
+// function to initialize app and generate svg file
+function init() {
+    inquirer.prompt(questions)
+    .then((data) => {
+
+        function renderShape() {
+            const userShape = data.logoShape;
+            if (userShape === 'Square') {
+                const userSquare = new Square();
+                userSquare.color = data.shapeColor;
+                return userSquare.render();
+            } else if (userShape === 'Triangle') {
+                const userTriangle = new Triangle();
+                userTriangle.color = data.shapeColor;
+                return userTriangle.render();
+            } else if (userShape === 'Circle') {
+                const userCircle = new Circle();
+                userCircle.color = data.shapeColor;
+                return userCircle.render();
+            } else {
+                console.log('Invalid Shape. No image generated')
+            }
+
+        }
+
+        function generateSVG(data) {
+            return `
+            <svg width="300" height="200" version="1.1" xmlns="http://www.w3.org/2000/svg">
+            ${renderShape()}
+            <text x="150" y="150" font-size="60" text-anchor="middle" fill="${data.textColor}">${data.logoText}</text>
+            </svg>`
+        }
+
+        fs.writeFile('./examples/logo.svg', generateSVG(data), (err) =>
+            err ? console.log(err) : console.log('Successfully created your svg file in examples folder. If you like the look of your newly generated logo, please save and rename the image.'))
+    });
+};
+
+init();
